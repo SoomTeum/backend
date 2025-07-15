048e211f3306960e61755cec927f2d8c925cc149
lilloo04 <lilloo04@naver.com>
2025-07-15 18:57:08 +0900

    chore: 초기 스프링 프로젝트 세팅 추가

---
+++ src/test/java/com/comma/soomteum/SoomteumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comma.soomteum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoomteumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
