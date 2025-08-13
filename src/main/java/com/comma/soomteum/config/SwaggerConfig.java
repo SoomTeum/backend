3778a58357218ea61f1c96c5b53e886879a7ddcf
김사라 <kimssirr@gachon.ac.kr>
2025-08-13 15:47:26 +0900

    [Feat/#20] 위치 기반 관광 조회 구현 (#22)
    * feat: 위치 기반 관광 조회 v1
    
    * feat: 위치 기반 관광 조회 v1
    
    ---------
    
    Co-authored-by: kimssirr <kimssirr@naver.com>

--- src/main/java/com/comma/soomteum/config/SwaggerConfig.java
+++ src/main/java/com/comma/soomteum/config/SwaggerConfig.java
@@ -1,6 +1,7 @@
 package com.comma.soomteum.config;
 
 
+
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
---
+++ src/main/java/com/comma/soomteum/domain/place/Config/TourApiProperties.java
@@ -0,0 +1,68 @@
+package com.comma.soomteum.domain.place.Config;
+
+
+import lombok.*;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ConfigurationProperties(prefix = "tourapi")
+public class TourApiProperties {
+
+    @Builder.Default
+    private Defaults defaults = new Defaults();
+    @Builder.Default
+    private Timeout timeout = new Timeout();
+    @Builder.Default
+    private Common common = new Common();
+    @Builder.Default
+    private Map<String, Client> clients = new HashMap<>();
+
+    @Getter
+    @Setter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Defaults {
+        private String serviceKey;
+    }
+
+    @Getter
+    @Setter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Timeout {
+        @Builder.Default
+        private int connectMs = 3000;
+        @Builder.Default
+        private int readMs = 5000;
+    }
+
+    @Getter
+    @Setter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Common {
+        private String mobileOs;
+        private String mobileApp;
+    }
+
+    @Getter
+    @Setter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Client {
+        private String baseUrl;
+        private String serviceKey;
+    }
+}
